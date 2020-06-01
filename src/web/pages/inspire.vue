ebce3ccf1a4f7ea1d5a456763e596db0e88be8b1
Phil Holden <phil.holden@outlook.com.au>
2020-06-01 21:54:13 +1000

    feat: use static web app setting

--- src/web/pages/inspire.vue
+++ src/web/pages/inspire.vue
@@ -3,7 +3,7 @@
     <v-flex class="text-center">
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;First, solve the problem. Then, write the code. {{ drawer }}&#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
@@ -13,3 +13,13 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: process.env.DUMMY || 'default'
+    }
+  }
+}
+</script>
