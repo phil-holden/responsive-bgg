3ed54703cdbb53516b601c6a0e52f5fcc54a7965
Phil Holden <phil.holden@outlook.com.au>
2020-06-22 10:23:36 +1000

    feat: add axios client

---
+++ src/api/Common/ApiClients.ts
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const BggClient = axios.create({
+  baseURL: process.env["BGG_API_HOST"],
+  responseType: 'json',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+const ImageClient = axios.create({
+  responseType: 'blob'
+});
+
+export { BggClient, ImageClient }
